e30212ddb19bad90d505bdadddc5f82afb156dc7
Pablo Sánchez <pablo_2jm@hotmail.com>
2022-10-20 10:09:47 +0200

    Format

--- pages/[...page].jsx
+++ pages/[...page].jsx
@@ -1,9 +1,13 @@
-import { useRouter } from 'next/router';
-import DefaultErrorPage from 'next/error';
-import Head from 'next/head';
-import React from 'react';
-import { BuilderComponent, builder, useIsPreviewing, Builder } from '@builder.io/react';
-
+import { useRouter } from "next/router";
+import DefaultErrorPage from "next/error";
+import Head from "next/head";
+import React from "react";
+import {
+  BuilderComponent,
+  builder,
+  useIsPreviewing,
+  Builder,
+} from "@builder.io/react";
 
 // Initialize the Builder SDK with your organization's API Key
 // Find the API Key on: https://builder.io/account/settings
@@ -14,9 +18,9 @@ export async function getStaticProps({ params }) {
   // Use the `userAttributes` field for targeting content.
   // For more, see https://www.builder.io/c/docs/targeting-with-builder
   const page = await builder
-    .get('page', {
+    .get("page", {
       userAttributes: {
-        urlPath: '/' + (params?.page?.join('/') || ''),
+        urlPath: "/" + (params?.page?.join("/") || ""),
       },
     })
     .toPromise();
@@ -33,14 +37,14 @@ export async function getStaticPaths() {
   //  Fetch all published pages for the current model.
   //  Using the `fields` option will limit the size of the response
   //  and only return the `data.url` field from the matching pages.
-  const pages = await builder.getAll('page', {
-    fields: 'data.url', // only request the `data.url` field
+  const pages = await builder.getAll("page", {
+    fields: "data.url", // only request the `data.url` field
     options: { noTargeting: true },
     limit: 0,
   });
 
   return {
-    paths: pages.map(page => `${page.data?.url}`),
+    paths: pages.map((page) => `${page.data?.url}`),
     fallback: true,
   };
 }
@@ -67,7 +71,7 @@ export default function Page({ page }) {
         <title>{page?.data.title}</title>
         <meta name="description" content={page?.data.descripton} />
       </Head>
-      <div style={{ padding: 50, textAlign: 'center' }}>
+      <div style={{ padding: 50, textAlign: "center" }}>
         {/* Put your header or main layout here */}
         Your header
       </div>
@@ -75,7 +79,7 @@ export default function Page({ page }) {
       {/* Render the Builder page */}
       <BuilderComponent model="page" content={page} />
 
-      <div style={{ padding: 50, textAlign: 'center' }}>
+      <div style={{ padding: 50, textAlign: "center" }}>
         {/* Put your footer or main layout here */}
         Your footer
       </div>
@@ -83,10 +87,10 @@ export default function Page({ page }) {
   );
 }
 
-//  This is an example of registering a custom component to be used in Builder.io. 
+//  This is an example of registering a custom component to be used in Builder.io.
 //  You would typically do this in the file where the component is defined.
 
-const MyCustomComponent = props => (
+const MyCustomComponent = (props) => (
   <div>
     <h1>{props.title}</h1>
     <p>{props.description}</p>
@@ -96,20 +100,24 @@ const MyCustomComponent = props => (
 //  This is a minimal example of a custom component, you can view more complex input types here:
 //  https://www.builder.io/c/docs/custom-react-components#input-types
 Builder.registerComponent(MyCustomComponent, {
-  name: 'ExampleCustomComponent',
+  name: "ExampleCustomComponent",
   inputs: [
-    { name: 'title', type: 'string', defaultValue: 'I am a React component! just testing' },
     {
-      name: 'description',
-      type: 'string',
-      defaultValue: 'Find my source in /pages/[...page].js',
+      name: "title",
+      type: "string",
+      defaultValue: "I am a React component! just testing",
+    },
+    {
+      name: "description",
+      type: "string",
+      defaultValue: "Find my source in /pages/[...page].js",
     },
   ],
 });
 
 // Register a custom insert menu to organize your custom componnets
 // https://www.builder.io/c/docs/custom-components-visual-editor#:~:text=than%20this%20screenshot.-,organizing%20your%20components%20in%20custom%20sections,-You%20can%20create
-Builder.register('insertMenu', {
-  name: 'My Components',
-  items: [{ item: 'ExampleCustomComponent', name: 'My React Component' }],
+Builder.register("insertMenu", {
+  name: "My Components",
+  items: [{ item: "ExampleCustomComponent", name: "My React Component" }],
 });
